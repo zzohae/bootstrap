3dcf9b51593bce80589b28aec62ad3ee7ceae015
zzohae <badahaeyeoncho@gmail.com>
2024-10-15 11:04:31 +0900

    기존 라디오 jquery 삭제

--- chy/form.js
+++
@@ -1,7 +0,0 @@
-$( function () {
-  $('input[name="options"]').on('change', function () {
-      $('.form-container').removeClass('d-block').addClass('d-none'); // 모든 폼 숨김
-      $('#form' + this.value).removeClass('d-none').addClass('d-block'); // 선택된 폼만 보임
-  });
-  $( "#inquiryType" ).selectmenu();
-});
